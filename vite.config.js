2fcb94ab07da4fd18f46978dac735e569a944c1b
hussaininamul25-whitespace <hussaininamul25@gmail.com>
2026-04-08 04:22:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,28 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-export default defineConfig({
+export default {
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        works: resolve(__dirname, 'works.html'),
-        project1: resolve(__dirname, 'project1.html'),
-        project2: resolve(__dirname, 'project2.html'),
-        project3: resolve(__dirname, 'project3.html'),
-        project4: resolve(__dirname, 'project4.html'),
-        project5: resolve(__dirname, 'project5.html'),
-        project6: resolve(__dirname, 'project6.html'),
-        project7: resolve(__dirname, 'project7.html'),
-        project8: resolve(__dirname, 'project8.html'),
-        project9: resolve(__dirname, 'project9.html'),
-        project10: resolve(__dirname, 'project10.html'),
-        project11: resolve(__dirname, 'project11.html'),
-        project12: resolve(__dirname, 'project12.html')
+        main: path.resolve(__dirname, 'index.html'),
+        works: path.resolve(__dirname, 'works.html'),
+        project1: path.resolve(__dirname, 'project1.html'),
+        project2: path.resolve(__dirname, 'project2.html'),
+        project3: path.resolve(__dirname, 'project3.html'),
+        project4: path.resolve(__dirname, 'project4.html'),
+        project5: path.resolve(__dirname, 'project5.html'),
+        project6: path.resolve(__dirname, 'project6.html'),
+        project7: path.resolve(__dirname, 'project7.html'),
+        project8: path.resolve(__dirname, 'project8.html'),
+        project9: path.resolve(__dirname, 'project9.html'),
+        project10: path.resolve(__dirname, 'project10.html'),
+        project11: path.resolve(__dirname, 'project11.html'),
+        project12: path.resolve(__dirname, 'project12.html')
       }
     }
   }
-});
+};
