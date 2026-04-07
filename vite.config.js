e9d861dc17b824d002d6122024f4fdf932100c1e
hussaininamul25-whitespace <hussaininamul25@gmail.com>
2026-04-08 04:14:58 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        works: resolve(__dirname, 'works.html'),
+        project1: resolve(__dirname, 'project1.html'),
+        project2: resolve(__dirname, 'project2.html'),
+        project3: resolve(__dirname, 'project3.html'),
+        project4: resolve(__dirname, 'project4.html'),
+        project5: resolve(__dirname, 'project5.html'),
+        project6: resolve(__dirname, 'project6.html'),
+        project7: resolve(__dirname, 'project7.html'),
+        project8: resolve(__dirname, 'project8.html'),
+        project9: resolve(__dirname, 'project9.html'),
+        project10: resolve(__dirname, 'project10.html'),
+        project11: resolve(__dirname, 'project11.html'),
+        project12: resolve(__dirname, 'project12.html')
+      }
+    }
+  }
+});
